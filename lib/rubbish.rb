ba2afc963cfea24f3ccd477bdb5881d1e4ac5ea4
kajisha <kajisha@gmail.com>
2017-04-04 23:32:48 +0900

    remove unnecessary path

--- lib/rubbish.rb
+++ lib/rubbish.rb
@@ -5,6 +5,6 @@ ActiveSupport::Dependencies.autoload_paths = ['./lib/']
 module Rubbish
   extend ActiveSupport::Autoload
 
-  autoload :Connection, './lib/rubbish/connection.rb'
-  autoload :Query, './lib/rubbish/query.rb'
+  autoload :Connection
+  autoload :Query
 end
